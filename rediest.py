66c7dad237e391e77d8928da146854e80c3e1a06
Tess REDIE <95612783+TessRedie@users.noreply.github.com>
2022-05-01 03:09:02 +0200

    Update rediest.py

--- rediest.py
+++ rediest.py
@@ -13,10 +13,15 @@ from pymongo import MongoClient
 #Connect to the server MongoDB
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 
+client = MongoClient()
+db = client['db']
+
 db = client["streamlipro"]
 mycollection = db['iris']
 st.write(mycollection)
 all_records = mycollection.find()
 
 list_cursor = list(all_records)
-data = pd.DataFrame(list_cursor)
+st.write(list_cursor)
+data = pd.DataFrame(list_cursor, columns= ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species'])
+st.write(data)
