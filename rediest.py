66b2a312172443b301ed64ad68419f7ae16d9a9d
Tess REDIE <95612783+TessRedie@users.noreply.github.com>
2022-05-01 02:47:22 +0200

    Update rediest.py

--- rediest.py
+++ rediest.py
@@ -12,19 +12,6 @@ from pymongo import MongoClient
 
 #Connect to the server MongoDB
 client = pymongo.MongoClient('mongodb://localhost:27017/')
-MONGODB_DATABASES = {
-    "default": {
-        "name": 'api',
-        "host": 'mongo',
-       "port": 27017
-    },
-}
-
-
-#create DB named 'MTSpam'
-#db = client['Iris']
-
-
 
 db = client["streamlipro"]
 mycollection = db['iris']
@@ -32,4 +19,4 @@ st.write(mycollection)
 all_records = mycollection.find()
 
 list_cursor = list(all_records)
-st.write(list_cursor)
+data = pd.DataFrame(list_cursor)
