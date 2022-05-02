6dc943a0cbd0104ed65058cfd3092d95417ef3b1
Tess REDIE <95612783+TessRedie@users.noreply.github.com>
2022-05-02 02:53:22 +0200

    Update redie.py

--- redie.py
+++ redie.py
@@ -66,10 +66,8 @@ from sklearn.preprocessing import FunctionTransformer
 import pymongo
 from pymongo import MongoClient
 
-
-#client = pymongo.MongoClient('mongodb://localhost:27017/')
 client=pymongo.MongoClient()
-client = MongoClient("mongodb+srv://localhost:27017/")
+client = pymongo.MongoClient('mongodb://localhost:27017/')
 db=client["streamlipro"]
 
 #db = client["streamlipro"]
