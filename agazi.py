ec29e18ffdf09953d12d760bf2a3979bc825a31c
Tess REDIE <95612783+TessRedie@users.noreply.github.com>
2022-05-03 17:12:55 +0200

    Update agazi.py

--- agazi.py
+++ agazi.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 from PIL import Image
-from PySide6 import QtDataVisualization #Q3DScatter
+from PySide6 import QtDataVisualization
 #libraries for text processing
 import nltk
 from nltk import FreqDist
